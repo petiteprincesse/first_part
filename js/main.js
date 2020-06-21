e95dba799e4e8ccfcf0a6c9e7471d51c012eaafe
Princess <Olgavm2007@yandex.ru>
2020-06-21 17:57:57 +0300

    some new methods have been used

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ let money,
 
 alert('Hello, World!');
 console.log('Hello, World!');
+
+money = 350000;
+income = 'Фриланс';
+addExpenses = 'Английский, фитнес, косметолог';
+deposit = true;
+mission = 3500000;
+period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
+
+let budgetDay;
+budgetDay = money/30;
+console.log(budgetDay);
