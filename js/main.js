58360f4e25002f1efc36e5887d3c1959a7852af6
Princess <Olgavm2007@yandex.ru>
2020-06-25 13:39:36 +0300

    feat: add text validation

--- js/main.js
+++ js/main.js
@@ -1,59 +1,14 @@
-let money = 350000,
-  income = "Фриланс",
-  addExpenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую"
-  ),
-  deposit = confirm("Есть ли у вас депозит в банке?"),
-  mission = 3500000,
-  period = 12;
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(", "));
-
-money = +prompt("Ваш месячный доход?", 150000);
-
-let budgetDay = money / 30;
-
-let expenses1 = prompt(
-    "Введите первую обязательную статью расходов?",
-    "Квартплата"
-  ),
-  amount1 = +prompt("Во сколько это обойдется?", "5000"),
-  expenses2 = prompt(
-    "Введите вторую обязательную статью расходов?",
-    "Интернет"
-  ),
-  amount2 = +prompt("Во сколько это обойдется?", "500");
-
-let expenses = amount1 + amount2;
-
-let budgetMonth = budgetDay * 30 - expenses;
-console.log(`Бюджет на месяц: ${budgetMonth}`);
-
-let purposeTime = mission / budgetMonth;
-purposeTime = Math.ceil(purposeTime);
-console.log(`Цель будет достигнута за ${purposeTime} месяцев`);
-
-budgetDay -= expenses / 30;
-console.log(`Бюджет на день: ${Math.floor(budgetDay)}`);
-
-if (budgetDay >= 1200) {
-  console.log("У вас высокий уровень дохода!");
-} else if (budgetDay > 600 && budgetDay < 1200) {
-  console.log("У вас средний уровень дохода");
-} else if (budgetDay <= 600 && budgetDay > 0) {
-  console.log("К сожалению у вас уровень дохода ниже среднего");
-} else if (budgetDay < 0) {
-  console.log("Что то пошло не так");
-} else if (budgetDay == 0) {
-  console.log("Вы не зарабатываете");
-} else {
-  console.log("Ошибка");
+'use strict';
+function argumentHandler(arg) {
+  if (typeof arg != "string") {
+    return "Введен некорректный тип данных";
+  }
+  arg = arg.trim();
+  // let argLength = arg.length;
+  if (arg.length > 30) {
+    arg = arg.substr(0, 30) + "... ";
+  }
+  return arg;
 }
+
+console.log(argumentHandler("Олечка"));
