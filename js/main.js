0a06800bb0dc9276417e12890c18cc33bed0b19d
Princess <Olgavm2007@yandex.ru>
2020-07-03 07:20:44 +0300

    feat: add two  analytics blocks

--- js/main.js
+++ js/main.js
@@ -1,98 +1,15 @@
 "use strict";
 
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-let money,
-  income = "Фриланс",
-  addExpenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую"
-  ),
-  deposit = confirm("Есть ли у вас депозит в банке?"),
-  mission = 3500000,
-  period = 12;
-
-let start = function () {
-  do {
-    money = prompt("Ваш месячный доход?");
-  } while (!isNumber(money));
-  // while (money === null || money.trim() === "" || isNaN(money)) {
-  //   money = prompt("Ваш месячный доход?");
-  // }
-};
-
-start();
-
-let showTypeOf = function (data) {
-  console.log(data, typeof data);
-};
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(", "));
-
-let expenses = [];
-let amount = [];
-
-function getExpensesMonth() {
-  let sum = 0;
-  for (let i = 0; i < 2; i++) {
-    expenses[i] = prompt("Введите обязательную статью расходов?");
-    do {
-      amount[i] = prompt("Во сколько это обойдётся?");
-    } while (!isNumber(amount[i]));
-    sum += +amount[i];
-  }
-  console.log(expenses);
-  return sum;
-}
-
-let expensesAmount = getExpensesMonth();
-
-console.log("Расходы за месяц: " + expensesAmount);
-
-function getAccumulatedMonth() {
-  return money - expensesAmount;
-}
-
-let accumulatedMonth = getAccumulatedMonth();
-// Бюджет на месяц без вынужденных трат
-
-function getTargetMonth() {
-  if (Math.ceil(mission / accumulatedMonth) < 0) {
-    return false;
-  } else {
-    return Math.ceil(mission / accumulatedMonth);
+let arr = [];
+arr = prompt("Введите семь любых многозначных чисел через один пробел").split(" ");
+for (let i=0; i<arr.length; i++) {
+  if (arr[i].startsWith("2") || arr[i].startsWith("4")) {
+    console.log(arr[i]);
   }
 }
 
-if (getTargetMonth()) {
-  console.log(`Цель будет достигнута за ${getTargetMonth()} месяцев`);
-} else {
-  console.log(`Цель не будет достигнута`);
-}
-
-let budgetDay = Math.floor(accumulatedMonth / 30);
-console.log(`Бюджет на день: ${budgetDay}`);
-
-let getStatusIncome = function () {
-  if (budgetDay >= 1200) {
-    return "У вас высокий уровень дохода!";
-  } else if (budgetDay > 600 && budgetDay < 1200) {
-    return "У вас средний уровень дохода";
-  } else if (budgetDay <= 600 && budgetDay > 0) {
-    return "К сожалению у вас уровень дохода ниже среднего";
-  } else if (budgetDay < 0) {
-    return "Что то пошло не так";
-  } else if (budgetDay == 0) {
-    return "Вы не зарабатываете";
-  } else {
-    return "Ошибка";
-  }
-};
-
-console.log(getStatusIncome());
+let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+for (let i=0; i<primeNumbers.length; i++) {
+  primeNumbers[i] += ` Делители этого числа 1 и ${primeNumbers[i]}`;
+  console.log(primeNumbers[i]);
+} 
